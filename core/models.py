d41a1102c61f48be696a47e209f85a1fec6a273f
PandaSkt <bray.pardo@duocuc.cl>
2021-07-24 21:58:52 -0400

    xd

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    idCategoria = models.IntegerField(primary_key=True)
+    nombreCategoria =models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombreCategoria
+
+class Vehiculo(models.Model):
+    patente = models.CharField(max_length=6, primary_key=True)
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=20, null=True, blank=True)
+    categoria =models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.patente
